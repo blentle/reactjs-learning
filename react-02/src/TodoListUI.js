82f0753d73aafe0ee6b028861eed8543ebc85b87
haun.ren <huan.ren@ipinyou.com>
2020-12-23 14:37:10 +0800

    split component

--- react-02/src/TodoListUI.js
+++ react-02/src/TodoListUI.js
@@ -1,27 +1,26 @@
-import React, { Component} from 'react';
+import React from 'react';
 import {Button, Input, List} from "antd";
-class TodoListUI extends Component {
-    render() {
-        return (
-            <div style={{marginLeft: '10px', marginTop: '10px'}}>
-                <div>
-                    <Input onChange={this.props.handleInputChange} placeholder="todo list"
-                           style={{width: '300px', marginRight: '10px'}} value={this.props.inputData}/>
-                    <Button type='primary' onClick={this.props.handleTaskAdd}>添加任务</Button>
-                </div>
-                <List
-                    style={{marginTop: '10px', width: '300px'}}
-                    size="small"
-                    bordered
-                    dataSource={this.props.dataList}
-                    renderItem={(item) => (
-                        <List.Item onClick= {(index)=> {this.props.handleDelete(index)}}>
-                            {item}
-                        </List.Item>
-                    )}
-                />
+const TodoListUI = (props) => {
+    return (
+        <div style={{marginLeft: '10px', marginTop: '10px'}}>
+            <div>
+                <Input onChange={props.handleInputChange} placeholder="todo list"
+                       style={{width: '300px', marginRight: '10px'}} value={props.inputData}/>
+                <Button type='primary' onClick={props.handleTaskAdd}>添加任务</Button>
             </div>
-        );
-    }
+            <List
+                style={{marginTop: '10px', width: '300px'}}
+                size="large"
+                bordered
+                dataSource={props.dataList}
+                renderItem={(item) => (
+                    <List.Item onClick= {(index)=> {props.handleDelete(index)}}>
+                        {item}
+                    </List.Item>
+                )}
+            />
+        </div>
+    );
 }
+
 export default TodoListUI;
